9776b9a0f82ff8d73981033669417d0615cd688f
Yield <83912710+yield02@users.noreply.github.com>
2022-08-10 14:11:29 +0700

    Reactive

---
+++ ReactJS/tiktok/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react'
+
+
+
+
+const courses = [
+  {
+    id: 1,
+    name: 'HTML, CSS'
+  },
+  {
+    id: 2,
+    name: 'Javascript'
+  },
+  {
+    id: 3,
+    name: 'ReactJS'
+  }
+]
+
+
+
+function App() {
+  
+
+  
+}
+
+export default App;
