e9fc910097366efbb78be2b7b969fa1c378d20c1
Yield <83912710+yield02@users.noreply.github.com>
2022-10-16 14:00:08 +0700

    Task

--- Project School/Nhập Môn WEb/Project_NM/js/script.js
+++ Project School/Nhập Môn WEb/Project_NM/js/script.js
@@ -7,17 +7,19 @@ import { $, $$} from './component/query.js'
 function Navbarindex() {
   return html`
     <ul class="nav-container">
-      <li class="nav-btn"><a href="#Header">Home</a></li>
+      <li class="nav-btn"><a href="/">Home</a></li>
       <li class="nav-btn"><a href="#concent">Concent</a></li>
       <li class="nav-btn"><a href="#footer">Footer</a></li>
-      <li class="nav-btn"><a href="./trangchu.html">Trang Chủ</a></li>
-      <li class="nav-btn"><a href="./dangnhap.html">Đăng Nhập</a></li>
       <li class="nav-btn"><a href="./taskjs.html">Tính Tam Giác và Giai thừa</a></li>
       <li class="nav-btn"><a href="./English.html">English</a></li>
     </ul>
   `;
 }
 
+
+
+      // <li class="nav-btn"><a href="./trangchu.html">Trang Chủ</a></li>
+      // <li class="nav-btn"><a href="./dangnhap.html">Đăng Nhập</a></li>
 
 
 
---
+++ Project School/Nhập Môn WEb/Project_NM/js/taskjs.js
@@ -0,0 +1,71 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const btn1 = $("#btn1");
+const btn2 = $("#btn2");
+const result1 = $("#result1");
+const result2 = $("#result2");
+
+
+function tinhtamgiac(a, b, c) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    c = parseFloat(c);
+    console.log(a+b);
+    if( a<b+c && b<a+c && c<a+b ) {
+        var p = a + b + c;
+        console.log("Chu vi tam giác là: ", p);
+        p = p / 2;
+        var s = Math.sqrt(p*(p - a)*(p-b)*(p-c));
+        console.log("Diện Tích Giam Giác là: ", s.toFixed(2));
+        result1.innerHTML = `Diện Tích Tam Giác: ${s.toFixed(2)}. Chu Vi Tam Giác: ${(p*2).toFixed(2)}`
+    }
+
+    else {
+        result1.innerHTML = 'Không phải là tam giác'
+        
+    }
+}
+
+
+function giaithua(n) {
+    if(n<=1) {
+        return 1;
+    }
+    return n * giaithua(n-1);
+}
+
+
+function handdleBtn2() {
+    var n = $('#n').value;
+    n = parseInt(n);
+    if(n) {
+        
+        console.log(giaithua(n));
+        result2.innerHTML = `Giai Thừa của số ${n} = ${giaithua(n)}`
+    }
+    else {
+        result2.innerHTML = `Vui lòng nhập n`
+
+    }
+}
+
+
+function handdleBtn1() {
+    var a = $('#a').value;
+    var b = $('#b').value;
+    var c = $('#c').value;
+
+    if(a, b, c) {
+        tinhtamgiac(a, b, c);
+    }
+    else {
+        result1.innerHTML = 'Vui lòng nhập đầy đủ a b c'
+
+    }
+}
+
+btn1.onclick = handdleBtn1;
+btn2.onclick = handdleBtn2;
+
+
